d5fb7ab524ce5a124bc4e1e84069745356aabe21
Simon Rapin <simon.rapin@gmail.com>
2016-05-07 14:09:30 +0200

    updated : single-product.php template

--- single-product.php
+++ single-product.php
@@ -1,12 +1,12 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <div class="row">
-	<div class="col-md-6">
+	<div class="col-md-8">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 		<?php endwhile; // end of the loop. ?>
 	</div>
-	<div class="col-md-6">
+	<div class="col-md-4">
 		<?php the_content(); ?>
 	</div>
 </div>
