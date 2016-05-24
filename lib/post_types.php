6b41880f420f9323e8142a6f5f54aff9dd15337a
Simon Rapin <simon.rapin@gmail.com>
2016-05-24 10:13:20 +0200

    updated : show deprecated post type "projet" in admin UI

--- lib/post_types.php
+++ lib/post_types.php
@@ -104,7 +104,7 @@ function grand_post_types() {
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
-		'show_in_menu'          => false,
+		'show_in_menu'          => true,
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
